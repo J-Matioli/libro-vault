84c0d773499d1b3530c26e23d75f6d33048e46fb
José Matioli <j-mateusalves@hotmail.com>
2023-06-26 13:39:41 -0300

    auth structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './features/login/login.component';
+import { LoginComponent } from './features/auth/login/login/login.component';
+
 
 
 const routes: Routes = [
