76c9bfbea0023948213d0a462a2ce1a3abbd887d
José Matioli <j-mateusalves@hotmail.com>
2023-06-26 22:47:14 -0300

    Create register-route

---
+++ src/app/features/auth/register/register.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void { }
+
+  submit(event: any) {
+    console.log(event);
+    this.router.navigate(['login'])
+  }
+}
