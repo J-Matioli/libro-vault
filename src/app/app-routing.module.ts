e18222726bec9a31a49d113129e5b73a343eec4a
José Matioli <j-mateusalves@hotmail.com>
2023-06-26 22:47:50 -0300

    Link to register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, data: { toolbar: false } },
+  { path: 'register', component: RegisterComponent, data: { toolbar: false } },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }
 ];
 
--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -6,16 +6,39 @@ import { MatInputModule } from '@angular/material/input';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginFormComponent } from './login/components/login-form/login-form.component';
 import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterFormComponent } from './register/components/register-form/register-form.component';
+import { RouterModule } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 
-
+const MY_DATE_FORMAT = {
+  parse: {
+    dateInput: 'DD/MM/YYYY', // this is how your date will be parsed from Input
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY', // this is how your date will get displayed on the Input
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY'
+  }
+};
 
 @NgModule({
   declarations: [
     LoginComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RegisterComponent,
+    RegisterFormComponent
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,    
@@ -24,6 +47,10 @@ import { LoginComponent } from './login/login/login.component';
   ],
   exports: [
     LoginComponent
+  ],
+  providers: [   
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMAT }
   ]
 })
 export class AuthModule { }
