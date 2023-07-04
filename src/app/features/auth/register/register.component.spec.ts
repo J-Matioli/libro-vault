eda315f09626fffc52ac4ed5d2e57b43263a751c
José Matioli <j-mateusalves@hotmail.com>
2023-07-04 09:55:14 -0300

    Updating register tests

--- src/app/features/auth/register/register.component.spec.ts
+++ src/app/features/auth/register/register.component.spec.ts
@@ -4,26 +4,46 @@ import { RegisterComponent } from './register.component';
 import { AuthModule } from '../auth.module';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 
 describe(RegisterComponent.name, () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ 
+      imports: [
         AuthModule,
         AppRoutingModule,
-        BrowserAnimationsModule ]
+        BrowserAnimationsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate when submit method is called', () => {
+
+    const formValue = {
+      nome: "Nome Sobrenome",
+      email: "teste@email.com",
+      dataDeNascimento:  "01/01/2000",
+      genero: "2",
+      senha: 'sdalk2312',
+      confirmarSenha:"teste@email.com"
+    }
+
+    const navigateSpy = spyOn(router, 'navigate');
+    component.submit(formValue);
+    expect(navigateSpy).toHaveBeenCalledWith(['login']);
+});
 });
