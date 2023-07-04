d36eacf1088be1d02845d914156c54e44192de78
José Matioli <j-mateusalves@hotmail.com>
2023-07-04 12:36:12 -0300

    Updating change password email tests

--- src/app/features/auth/change-password-email/components/change-password-email-form/change-password-email-form.component.spec.ts
+++ src/app/features/auth/change-password-email/components/change-password-email-form/change-password-email-form.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangePasswordEmailFormComponent } from './change-password-email-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthModule } from '../../../auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -25,5 +24,23 @@ describe(ChangePasswordEmailFormComponent.name, () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should NOT emit form value if form is valid', () => {
+    const fomrEmitterSpy = spyOn(component.formSubmit, 'emit');
+
+    component.submit();
+    expect(fomrEmitterSpy).not.toHaveBeenCalled();
+  });
+
+  it('should emit form value if form is valid', () => {
+    const fomrEmitterSpy = spyOn(component.formSubmit, 'emit');
+
+    component.form.setValue({
+      email: 'testemail@teste.com',
+    })
+
+    component.submit();
+    expect(fomrEmitterSpy).toHaveBeenCalled();
   });
 });
