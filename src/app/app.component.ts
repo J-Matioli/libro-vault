3352f5c52ef8082d238688a2ac261e5a64f258f2
José Matioli <j-mateusalves@hotmail.com>
2023-07-11 21:16:55 -0300

    Sidebar display logic in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { MatDrawerMode } from '@angular/material/sidenav';
+import { Router, RoutesRecognized } from '@angular/router';
 import { Observable, map } from 'rxjs';
 
 @Component({
@@ -11,12 +12,26 @@ import { Observable, map } from 'rxjs';
 export class AppComponent implements OnInit {
   
   public sidenavType!: Observable<MatDrawerMode>;
+  public displayNavbar!: boolean;
 
-  constructor(breakpointObserver: BreakpointObserver) { 
+  constructor(
+    breakpointObserver: BreakpointObserver,
+    private router: Router) { 
       this.sidenavType = breakpointObserver
       .observe('(min-width: 600px)')
       .pipe(map(({matches}) => matches ? 'side' : 'over'));
   }
   
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.verifyRouteToShowSidenav();
+  }
+
+  verifyRouteToShowSidenav() {
+    this.router.events.subscribe(event => {
+      if(event instanceof RoutesRecognized) {
+        let route = event.state.root.firstChild;        
+        this.displayNavbar = route?.data['toolbar'] ?? true;
+      }
+    })
+  }
 }
