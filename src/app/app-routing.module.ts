63b91d959203d50162494f11d8c378e76690001b
José Matioli <j-mateusalves@hotmail.com>
2023-07-14 17:53:54 -0300

    Routes Error Handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { ChangePasswordEmailComponent } from './features/auth/change-password-em
 import { ChangePasswordComponent } from './features/auth/change-password/change-password.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, data: { toolbar: false, footer: false  } },
   { path: 'register', component: RegisterComponent, data: { toolbar: false, footer: false  } },
   { path: 'change-password-email', component: ChangePasswordEmailComponent, data: { toolbar: false, footer: false  } },
   { path: 'change-password', component: ChangePasswordComponent, data: { toolbar: false, footer: false  } },
-  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }
+  { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
