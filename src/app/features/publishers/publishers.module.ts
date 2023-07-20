7e5b4dbcaa0242263594905330d6babfa2fafca5
José Matioli <j-mateusalves@hotmail.com>
2023-07-20 17:07:40 -0300

    Inserting Custom Title Component on Publisher Route

--- src/app/features/publishers/publishers.module.ts
+++ src/app/features/publishers/publishers.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublishersComponent } from './publishers/publishers.component';
 import { PublishersRoutingModule } from './publishers-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -11,6 +12,7 @@ import { PublishersRoutingModule } from './publishers-routing.module';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     PublishersRoutingModule
   ]
 })
