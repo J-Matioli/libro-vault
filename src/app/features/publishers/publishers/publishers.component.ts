641a8c922f1df19b99647c74768461d322ec0700
José Matioli <j-mateusalves@hotmail.com>
2023-07-21 13:46:01 -0300

    Inserting table in Publishers route

--- src/app/features/publishers/publishers/publishers.component.ts
+++ src/app/features/publishers/publishers/publishers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-publishers',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublishersComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public tableHeaders = { 
+    id: 'Id',
+    name: 'Editora',
+    qtdObras: 'Qtd. Obras'
   }
 
+  public publishers = [
+  { id: 1, name: 'Panini', qtdObras: 7 },
+  { id: 2, name: 'JBC', qtdObras: 4 },
+  { id: 3, name: 'Pipoca e Nanquim', qtdObras: 3 },
+  { id: 4, name: 'Devir', qtdObras: 2 },
+  { id: 5, name: 'NewPop', qtdObras: 1 },
+  { id: 6, name: 'DarkSide', qtdObras: 1 },
+  { id: 7, name: 'L&PM', qtdObras: 2 },
+  { id: 8, name: 'Companhia das Letras', qtdObras: 3 },
+  { id: 9, name: 'SESI', qtdObras: 2 },
+  { id: 10, name: 'Rocco', qtdObras: 1 }
+] 
+
+ public pageSettings: PageEvent = { length: 10, pageIndex: 0, pageSize: 10 }
+
+constructor() { }
+
+ngOnInit(): void {}
+
+userAction(ev: any) {
+  console.log(ev)
+}
+
+searchAction(ev: any) {
+  console.log(ev)
+}
+
+pageAction(ev: any) {
+  console.log(ev)
+}
+  
 }
