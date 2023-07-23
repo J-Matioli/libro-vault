d47739b7b8464cc13a3ec9e4ea45ec36079690d6
José Matioli <j-mateusalves@hotmail.com>
2023-07-23 12:43:06 -0300

    Concluding PublisherFormDialog

--- src/app/shared/publisher-form-dialog/publisher-form-dialog.component.ts
+++ src/app/shared/publisher-form-dialog/publisher-form-dialog.component.ts
@@ -2,10 +2,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-export interface DialogData {
+export interface PublisherDialogData {
   action: 'ADD' | 'UPDATE' | 'DELETE';
   publisher: any;
-  name: string;
 }
 
 @Component({
@@ -14,37 +13,52 @@ export interface DialogData {
   styleUrls: ['./publisher-form-dialog.component.scss']
 })
 export class PublisherFormDialogComponent implements OnInit {
-
+  public actions: typeof Actions = Actions;
   public form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+    @Inject(MAT_DIALOG_DATA) public data: PublisherDialogData) { }
 
   ngOnInit(): void {
-    this.createForm();    
+    this.createForm();
+    if(this.data.action === 'UPDATE') {
+      this.populateForm();
+    }
   }
 
-  get editora() { return this.form.get('editora'); }  
+  get editora() { return this.form.get('editora'); }
 
   createForm(): void {
     this.form = this.fb.group<PublisherForm>({
-      editora: new FormControl(null, {validators: [Validators.required]}),
+      editora: new FormControl(null, { validators: [Validators.required] }),
+    })
+  }
+
+  populateForm() {
+    this.form.patchValue({
+      editora: this.data.publisher.name
     })
   }
 
   submit() {
     this.form.markAllAsTouched();
-    if(this.form.valid) {     
-      
+    if (this.form.valid) {
+      console.log(this.data.action, this.form.value)
     }
   }
 
   deletePublisher() {
-
+    console.log(this.data.action, this.data.publisher.id)
   }
 }
 
 export interface PublisherForm {
-  editora: FormControl<string | null>  
+  editora: FormControl<string | null>
+}
+
+export enum Actions {
+  'ADD' = 'Adicionar',
+  'UPDATE' = 'Atualizar',
+  'DELETE' = 'Remover'
 }
