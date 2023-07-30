7787a9bd5cbe3d17b7a6b677f6a12100811a7021
José Matioli <j-mateusalves@hotmail.com>
2023-07-30 11:58:18 -0300

    Form modals

--- src/app/shared/custom-form/custom-form.component.ts
+++ src/app/shared/custom-form/custom-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthorFormDialogComponent } from '../author-form-dialog/author-form-dialog.component';
+import { PublisherFormDialogComponent } from '../publisher-form-dialog/publisher-form-dialog.component';
+import { GenreFormDialogComponent } from '../genre-form-dialog/genre-form-dialog.component';
 
 @Component({
   selector: 'app-custom-form',
@@ -9,7 +13,7 @@ export class CustomFormComponent implements OnInit {
 
   @Input() workType: 'livro' | 'manga' | 'hq';
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
@@ -20,4 +24,29 @@ export class CustomFormComponent implements OnInit {
 
   addVol() {}
   removeVol(i: number) {}
+
+  addAuthor() {
+      const dialogRef = this.dialog.open(AuthorFormDialogComponent, {
+        restoreFocus: false,
+        data: {
+          action: 'ADD'
+        },
+      });
+  }
+  addPublisher() {
+    const dialogRef = this.dialog.open(PublisherFormDialogComponent, {
+      restoreFocus: false,
+      data: {
+        action: 'ADD'
+      },
+    });
+  }
+  addGenre() {
+    const dialogRef = this.dialog.open(GenreFormDialogComponent, {
+      restoreFocus: false,
+      data: {
+        action: 'ADD'
+      },
+    });
+  }
 }
