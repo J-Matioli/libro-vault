2c8ee5fbcbf30e8a6bf8f73f9a12c47e7d4ca749
José Matioli <j-mateusalves@hotmail.com>
2023-07-30 22:50:17 -0300

    Child FormGroup

--- src/app/shared/custom-form/components/more-info-form/more-info-form.component.ts
+++ src/app/shared/custom-form/components/more-info-form/more-info-form.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ControlContainer, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-more-info-form',
+  selector: '[formGroup] app-more-info-form, [formGroupName] app-more-info-form',
   templateUrl: './more-info-form.component.html',
   styleUrls: ['./more-info-form.component.scss']
 })
 export class MoreInfoFormComponent implements OnInit {
 
-  constructor() { }
+  public moreInfoForm: FormGroup;
+
+  constructor(private controlContainer: ControlContainer) { }
 
   ngOnInit(): void {
+    this.moreInfoForm = this.controlContainer.control as FormGroup;
   }
 
 }
--- src/app/shared/custom-form/custom-form.component.ts
+++ src/app/shared/custom-form/custom-form.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { AuthorFormDialogComponent } from '../author-form-dialog/author-form-dialog.component';
 import { PublisherFormDialogComponent } from '../publisher-form-dialog/publisher-form-dialog.component';
 import { GenreFormDialogComponent } from '../genre-form-dialog/genre-form-dialog.component';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { genresOptions, languageOptions, authorOptions, publisherOptions } from '../custom-filter/filter-helper';
 
 @Component({
   selector: 'app-custom-form',
@@ -11,11 +13,41 @@ import { GenreFormDialogComponent } from '../genre-form-dialog/genre-form-dialog
 })
 export class CustomFormComponent implements OnInit {
 
+  public form: FormGroup<CustomForm>;
+
+  public genresOptions = genresOptions;
+  public languageOptions = languageOptions;
+  public authorOptions = authorOptions;
+  public publisherOptions = publisherOptions;
+
   @Input() workType: 'livro' | 'manga' | 'hq';
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+
+    this.form.valueChanges.subscribe(console.log)
+  }
+
+  createForm() {
+    this.form = this.fb.group<CustomForm>({
+      obra: new FormControl(),
+      autor: new FormControl(),
+      editora: new FormControl(),
+      idioma: new FormControl(),
+      anotacoes: new FormControl(),
+      generos: new FormControl(),
+      volumeUnico: new FormControl(),
+      maisInfo: new FormGroup({
+        preco: new FormControl(),
+        pagina: new FormControl(),
+        lido: new FormControl(),
+      })
+    })
   }
 
   croppedImage(ev: any) {
@@ -49,4 +81,51 @@ export class CustomFormComponent implements OnInit {
       },
     });
   }
+
+  displayAutoComplete(option: any): string {       
+    return option && option.viewValue ? option.viewValue : '';
+  }
+  
+  get obra(): FormControl {
+    return this.form.get('obra') as FormControl
+  }
+  
+  get autor(): FormControl {
+    return this.form.get('autor') as FormControl
+  }
+  
+  get editora(): FormControl {
+    return this.form.get('editora') as FormControl
+  }
+
+  get idioma(): FormControl {
+    return this.form.get('idioma') as FormControl
+  }
+
+  get anotacoes(): FormControl {
+    return this.form.get('anotacoes') as FormControl
+  }
+
+  get generos(): FormControl {
+    return this.form.get('generos') as FormControl
+  }
+
+  get volumeUnico(): FormControl {
+    return this.form.get('volumeUnico') as FormControl
+  }
+
+  get maisInfo(): FormGroup {
+    return this.form.get('maisInfo') as FormGroup
+  }
+}
+
+export interface CustomForm {
+  obra: FormControl<string>
+  autor: FormControl<string[]>
+  editora: FormControl<string>  
+  idioma: FormControl<string[]>
+  anotacoes: FormControl<string>
+  generos: FormControl<string[]>
+  volumeUnico: FormControl<string>
+  maisInfo: FormGroup
 }
