869c60d2ce4bded6c5ba3a9439f43825c611bc26
José Matioli <j-mateusalves@hotmail.com>
2023-08-08 17:38:02 -0300

    Fix - Manga volumes

--- src/app/features/mangas/mangas-details/mangas-details.component.ts
+++ src/app/features/mangas/mangas-details/mangas-details.component.ts
@@ -32,67 +32,84 @@ export class MangasDetailsComponent implements OnInit {
           title: 'Akira Vol 1',
           author: ['Katsuhiro Otomo'],
           pages: 438,
-          read: true,        
+          read: true,
           buyDate: new Date(2020, 9, 13),
           genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
-        img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+          img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
         },
         {
           id: '2',
           title: 'Akira Vol 2',
           author: ['Katsuhiro Otomo'],
           pages: 438,
-          read: true,        
+          read: true,
           buyDate: new Date(2020, 9, 13),
           genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
-        img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+          img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
         },
         {
           id: '3',
           title: 'Akira Vol 3',
           author: ['Katsuhiro Otomo'],
           pages: 438,
-          read: true,        
+          read: true,
           buyDate: new Date(2020, 9, 13),
           genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
-        img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+          img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
         },
         {
           id: '4',
           title: 'Akira Vol 4',
           author: ['Katsuhiro Otomo'],
           pages: 438,
-          read: true,        
+          read: true,
           buyDate: new Date(2020, 9, 13),
           genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
-        img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+          img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
         },
         {
           id: '5',
           title: 'Akira Vol 5',
           author: ['Katsuhiro Otomo'],
           pages: 438,
-          read: true,        
+          read: true,
           buyDate: new Date(2020, 9, 13),
           genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
-        img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+          img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
         },
         {
           id: '6',
           title: 'Akira Vol 6',
           author: ['Katsuhiro Otomo'],
           pages: 438,
-          read: true,        
+          read: true,
           buyDate: new Date(2020, 9, 13),
           genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
-        img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+          img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
         }
       ],
       anotacoes: 'Akira é um dos marcos da ficção científica oriental e revolucionou a chegada dos mangás e da cultura pop japonesa no Ocidente. Ambientada em um cenário pós-apocalíptico da Neo Tokyo, 30 anos depois da III Guerra Mundial, a história é centrada nos personagens Kaneda e Tetsuo, membros de uma gangue de motoqueiros que se deparam com o poder sobrenatural de Akira. A obra foi escrita e ilustrada por Katsuhiro Otomo e tornou-se uma das principais referências da onda cyberpunk década de 1980, ao lado de grandes títulos como Neuromacer, de William Gibson.',
       buyDate: null,
       readDate: null,
       genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
-    img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+      img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
+    },
+    volume: {
+      mangId: '3',
+      id: '2',
+      title: 'Akira',
+      number: 1,
+      author: ['Katsuhiro Otomo'],
+      publisher: 'JBC',
+      language: ['Português'],
+      price: '230',
+      pages: '2628',
+      read: true,
+      anotacoes: 'Akira é um dos marcos da ficção científica oriental e revolucionou a chegada dos mangás e da cultura pop japonesa no Ocidente. Ambientada em um cenário pós-apocalíptico da Neo Tokyo, 30 anos depois da III Guerra Mundial, a história é centrada nos personagens Kaneda e Tetsuo, membros de uma gangue de motoqueiros que se deparam com o poder sobrenatural de Akira. A obra foi escrita e ilustrada por Katsuhiro Otomo e tornou-se uma das principais referências da onda cyberpunk década de 1980, ao lado de grandes títulos como Neuromacer, de William Gibson.',
+      buyDate: new Date(),
+      readDate: new Date(),
+      genres: ['Cyberpunk', 'Thriller político', 'Ficção científica'],
+      img: 'https://upload.wikimedia.org/wikipedia/it/2/29/Akira_film.jpg'
     }
   };
 
@@ -105,16 +122,16 @@ export class MangasDetailsComponent implements OnInit {
     private dialog: MatDialog) { }
 
   ngOnInit(): void {
-     this.workTypeVolume = this.route.snapshot.queryParams['vol'] ? true : false;
-   }
+    this.workTypeVolume = this.route.snapshot.queryParams['vol'] ? true : false;
+  }
 
   edit() {
-    this.router.navigate(['../../editar', this.mangaId], {relativeTo: this.route});
+    this.router.navigate(['../../editar', this.mangaId], { relativeTo: this.route });
   }
 
   onBackBtn() {
-    this.workTypeVolume? 
-      this.router.navigate(['../../detalhes', this.mangaId], {relativeTo: this.route})
+    this.workTypeVolume ?
+      this.router.navigate(['../../detalhes', this.mangaId], { relativeTo: this.route })
       : this.router.navigate(['mangas']);
   }
 
@@ -124,9 +141,9 @@ export class MangasDetailsComponent implements OnInit {
         this.router.routeReuseStrategy.shouldReuseRoute = function () {
           return false
         }
-    
+
         this.router.onSameUrlNavigation = 'reload';
-        this.router.navigate(['../../detalhes', this.mangaId], {relativeTo: this.route, queryParams: {vol: ev.id} });
+        this.router.navigate(['../../detalhes', this.mangaId], { relativeTo: this.route, queryParams: { vol: ev.id } });
         break;
       case 'EDIT':
         this.edit();
@@ -156,7 +173,7 @@ export class MangasDetailsComponent implements OnInit {
 
   pageChanged(pageEvent: PageEvent) {
     if (!pageEvent) {
-        return;
+      return;
     }
     console.log(pageEvent)
   }
