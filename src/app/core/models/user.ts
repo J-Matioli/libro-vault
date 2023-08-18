5c8d4b199a30f2686480ff2c57615bb63216312a
José Matioli <j-mateusalves@hotmail.com>
2023-08-18 17:00:15 -0300

    User model

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -8,3 +8,21 @@ export interface User {
     notificar: boolean
     status: string
 }
+
+export interface UserResponse {
+    dados: {
+        dados: Dados,
+        pagina: User[]
+    }
+    mensagem: string[];
+    sucesso: boolean
+}
+
+export interface Dados {
+    paginaAtual: number
+    totalPaginas: number
+    resultadosExibidosPagina: number
+    contagemTotalResultados: number
+    existePaginaAnterior: boolean
+    existePaginaPosterior: boolean
+}
