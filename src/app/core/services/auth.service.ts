3679a77da20b936d05f7d75f300cfa2cea6f308a
José Matioli <j-mateusalves@hotmail.com>
2023-08-18 17:00:46 -0300

    User requests and header

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable, catchError, throwError } from 'rxjs';
 import { AuthResponse } from '../models/auth';
+import { User, UserResponse } from '../models/user';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,8 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private cookieService: CookieService
   ) { }
 
   login(data: {email: string, senha: string}): Observable<AuthResponse> {
@@ -62,6 +65,33 @@ export class AuthService {
       this.openSnackBar(err.error.erros[0])
       return throwError(() => err)
     }))
+  }
+
+  getUser(): Observable<UserResponse> {
+    const id = this.cookieService.get('_id')
+    return this.http.get<UserResponse>(`${this.apiUrl}usuario/usuario?id=${id}`, this.authHeader())
+    .pipe(catchError(err => {
+      this.openSnackBar(err.error.erros[0])
+      return throwError(() => err)
+    }))
+  }
+
+  putUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}usuario/atualiza-usuario`, user, this.authHeader())
+    .pipe(catchError(err => {
+      this.openSnackBar(err.error.erros[0])
+      return throwError(() => err)
+    }))
+  }
+
+  authHeader() {
+    const token = this.cookieService.get('_token');
+
+    return {
+      headers: new HttpHeaders({
+        "authorization": `bearer ${token}`
+      })
+    };
   }
 
   openSnackBar(message: string) {
