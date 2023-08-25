17a01455538a7abcce3a381ba0d80989fcc64d39
José Matioli <j-mateusalves@hotmail.com>
2023-08-25 13:46:10 -0300

    Fix - Filter spec

--- src/app/shared/custom-filter/custom-filter.component.spec.ts
+++ src/app/shared/custom-filter/custom-filter.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomFilterComponent } from './custom-filter.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('CustomFilterComponent', () => {
+describe(CustomFilterComponent.name, () => {
   let component: CustomFilterComponent;
   let fixture: ComponentFixture<CustomFilterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomFilterComponent ]
+      declarations: [ CustomFilterComponent ],
+      imports: [
+        MatFormFieldModule,
+        MatChipsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CdkAccordionModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
 
