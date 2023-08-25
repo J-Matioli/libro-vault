114e8c9a190b6514ee6052592bb74d875aa6cc5f
José Matioli <j-mateusalves@hotmail.com>
2023-08-25 16:46:13 -0300

    Fix - GenreFormDialogComponent specs

--- src/app/shared/genre-form-dialog/genre-form-dialog.component.spec.ts
+++ src/app/shared/genre-form-dialog/genre-form-dialog.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GenreFormDialogComponent } from './genre-form-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
-describe('GenreFormDialogComponent', () => {
+describe(GenreFormDialogComponent.name, () => {
   let component: GenreFormDialogComponent;
   let fixture: ComponentFixture<GenreFormDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GenreFormDialogComponent ]
+      declarations: [ GenreFormDialogComponent ],
+      imports: [ 
+        ReactiveFormsModule,
+        MatDialogModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(GenreFormDialogComponent);
     component = fixture.componentInstance;
+    
     fixture.detectChanges();
   });
 
