1da82582cec464e739f7016329b59c64fd945692
José Matioli <j-mateusalves@hotmail.com>
2023-08-25 16:52:06 -0300

    Fix - CustomFormComponent specs

--- src/app/shared/custom-form/custom-form.component.spec.ts
+++ src/app/shared/custom-form/custom-form.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomFormComponent } from './custom-form.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
-describe('CustomFormComponent', () => {
+describe(CustomFormComponent.name, () => {
   let component: CustomFormComponent;
   let fixture: ComponentFixture<CustomFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomFormComponent ]
+      declarations: [ CustomFormComponent ],
+      imports: [ 
+        MatDialogModule,
+        ReactiveFormsModule,
+        MatAutocompleteModule,
+        MatCheckboxModule
+      ]
     })
     .compileComponents();
 
