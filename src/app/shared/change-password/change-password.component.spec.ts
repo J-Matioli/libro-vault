4dd254f621d20db62767cc5b956220d6aa133ef9
José Matioli <j-mateusalves@hotmail.com>
2023-08-28 15:38:20 -0300

    Fix - ChangePasswordComponent specs

--- src/app/shared/change-password/change-password.component.spec.ts
+++ src/app/shared/change-password/change-password.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangePasswordComponent } from './change-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
 
-describe('ChangePasswordComponent', () => {
+describe(ChangePasswordComponent.name, () => {
   let component: ChangePasswordComponent;
   let fixture: ComponentFixture<ChangePasswordComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChangePasswordComponent ]
+      declarations: [ ChangePasswordComponent ],
+      imports: [
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        HttpClientModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
     .compileComponents();
 
