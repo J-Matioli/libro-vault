47c43ad59999d33b6c781b848aa343e33f991434
José Matioli <j-mateusalves@hotmail.com>
2023-08-28 18:34:17 -0300

    Fix - BooksDetailsComponent specs

--- src/app/features/books/books-details/books-details.component.spec.ts
+++ src/app/features/books/books-details/books-details.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BooksDetailsComponent } from './books-details.component';
+import { BooksModule } from '../books.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
-describe('BooksDetailsComponent', () => {
+describe(BooksDetailsComponent.name, () => {
   let component: BooksDetailsComponent;
   let fixture: ComponentFixture<BooksDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BooksDetailsComponent ]
+      imports: [ 
+        BooksModule,
+        AppRoutingModule
+      ]
     })
     .compileComponents();
 
