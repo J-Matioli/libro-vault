c1afb652293b731fa37186f699b40c7ce4c2007a
José Matioli <j-mateusalves@hotmail.com>
2023-08-28 15:54:03 -0300

    Fix - CustomCardComponent specs

--- src/app/shared/custom-card/custom-card.component.spec.ts
+++ src/app/shared/custom-card/custom-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomCardComponent } from './custom-card.component';
+import { ArrToStringPipe } from 'src/app/core/pipes/arr-to-string.pipe';
 
 describe(CustomCardComponent.name, () => {
   let component: CustomCardComponent;
@@ -8,7 +9,10 @@ describe(CustomCardComponent.name, () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomCardComponent ]
+      declarations: [ 
+        CustomCardComponent,
+        ArrToStringPipe
+      ]
     })
     .compileComponents();
 
