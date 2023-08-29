5447ad659fd0168fe2de57075029debf7940a8ee
José Matioli <j-mateusalves@hotmail.com>
2023-08-29 15:36:36 -0300

    Fix - VolumeFormComponent specs

--- src/app/shared/custom-form/components/volume-form/volume-form.component.spec.ts
+++ src/app/shared/custom-form/components/volume-form/volume-form.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VolumeFormComponent } from './volume-form.component';
+import { ControlContainer, FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CustomVolumeForm } from 'src/app/core/utils/form-utils';
 
-describe('VolumeFormComponent', () => {
+describe(VolumeFormComponent.name, () => {
   let component: VolumeFormComponent;
   let fixture: ComponentFixture<VolumeFormComponent>;
 
+  const fg: FormGroup<CustomVolumeForm> = new FormGroup<CustomVolumeForm>({
+    volume: new FormControl(''),
+    anotacoes: new FormControl(''),
+    imagem: new FormControl(''),
+    maisInfo: new FormGroup({
+      dataCompra: new FormControl(''),
+      dataLeitura: new FormControl(''),
+      lido: new FormControl(''),
+      pagina: new FormControl(''),
+      preco: new FormControl('')
+    })
+  });
+
+  const fgd: FormGroupDirective = new FormGroupDirective([], []);
+  fgd.form = fg;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VolumeFormComponent ]
+      declarations: [ VolumeFormComponent ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        {provide: ControlContainer, useValue: fgd}
+      ]
     })
     .compileComponents();
 
