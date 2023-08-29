f7aaf9a2cd071d57ec1d533a11404a1c6c341ce2
José Matioli <j-mateusalves@hotmail.com>
2023-08-28 21:40:02 -0300

    Fix - Genres, Authors and Publishers specs

--- src/app/features/genres/genres.component.spec.ts
+++ src/app/features/genres/genres.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GenresComponent } from './genres.component';
+import { GenresModule } from './genres.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('GenresComponent', () => {
+describe(GenresComponent.name, () => {
   let component: GenresComponent;
   let fixture: ComponentFixture<GenresComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GenresComponent ]
+      imports: [ 
+        GenresModule,
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
 
