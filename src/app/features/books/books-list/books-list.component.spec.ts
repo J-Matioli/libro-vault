b4ad461ac0fcb59a4d73187120fb166f4a9f2f54
José Matioli <j-mateusalves@hotmail.com>
2023-09-06 17:59:33 -0300

    Add tests in BooksListComponent

--- src/app/features/books/books-list/books-list.component.spec.ts
+++ src/app/features/books/books-list/books-list.component.spec.ts
@@ -5,10 +5,13 @@ import { BooksModule } from '../books.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router, ActivatedRoute } from '@angular/router';
 
 describe(BooksListComponent.name, () => {
   let component: BooksListComponent;
   let fixture: ComponentFixture<BooksListComponent>;
+  let router: Router;
+  let route: ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -23,10 +26,76 @@ describe(BooksListComponent.name, () => {
 
     fixture = TestBed.createComponent(BooksListComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    route = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`#${BooksListComponent.prototype.cardAction.name}
+  should navigate to details on event action`, () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    const books = component.books;
+
+    for(let i = 0; i < books.length; i++) {
+
+      const ev = { 
+        action: 'GET',
+        id: books[i].id
+      }
+
+      component.cardAction(ev)
+            
+      expect(navigateSpy)
+        .withContext(books[i].title)
+        .toHaveBeenCalledWith(
+          [
+            './detalhes',
+            ev.id
+          ],
+          {relativeTo: route
+        });
+    }  
+  })
+
+  it(`#${BooksListComponent.prototype.cardAction.name}
+  should navigate to edit on event action`, () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    const books = component.books;
+
+    for(let i = 0; i < books.length; i++) {
+
+      const ev = { 
+        action: 'EDIT',
+        id: books[i].id
+      }
+
+      component.cardAction(ev)
+            
+      expect(navigateSpy)
+        .withContext(books[i].title)
+        .toHaveBeenCalledWith(
+          [
+            './editar',
+            ev.id
+          ],
+          {relativeTo: route
+        });
+    }  
+  })
+
+  it(`#${BooksListComponent.prototype.addBookRoute.name}
+  should navigate to Add when is method is called`, () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    
+      component.addBookRoute()
+            
+      expect(navigateSpy)    
+        .toHaveBeenCalledWith(
+          ['./adicionar'], {relativeTo: route}
+        );
+  })
 });
